8a601b1f2a63a9186d5586beab0cdbd9779c8dc0
Ramiz Raja <informramiz@gmail.com>
2024-03-24 02:46:23 +0500

    chore: Add mlkit document scanner API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    // Main Android Libs
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +60,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Document Scanner
+    implementation(libs.mlkit.document.scanner)
+
+    // Test and Debug Libraries
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
